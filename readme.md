21e719e87bb12eb9bff3d5206a5f3990bb467d29
Jeffrey Skinner <jeff@je.gy>
2023-09-08 03:11:33 +0900

    First and last commit?

---
+++ readme.md
@@ -0,0 +1,16 @@
+## Bright - Rust Backlight Controller for Linux
+
+# Usage
+Bright is a Rust program designed to help you control the backlight brightness on your Linux system.
+Please ensure that your backlight is exposed in userspace at the /sys/backlight/ directory.
+
+To adjust the brightness, follow these steps:
+1. Make sure you have superuser privileges. You can use the sudo command to run Bright with elevated permissions.
+2. Open your terminal and use the following command, replacing <BRIGHTNESS_SCALE> with your desired brightness level:
+
+```sh
+sudo bright <BRIGHTNESS_SCALE>
+```
+
+BRIGHTNESS_SCALE is a value between 0 and 1, allowing you to set the brightness level anywhere from black to maximum brightness.
+Please keep in mind that this is my first attempt at creating a Rust program, so it may have some room for improvement. Your feedback and contributions are welcome!
---
+++ src/main.rs
@@ -0,0 +1,96 @@
+fn constant_speed_interpolation(p: f32, t: f32, s: f32, dt: f32) -> f32 {
+	let o = t - p;
+	let z = dt*s;
+
+	let mut u = 0.0;
+	if o > 0.0 {
+		u = 1.0;
+	}
+	else {
+		u = -1.0;
+	}
+
+	if z*z < o*o {
+		return p + z*u;
+	}
+
+	return t;
+}
+
+fn bv_to_lum(bv: f32) -> f32 {
+	return bv.powf(1.0/3.0);
+}
+
+fn lum_to_bv(lum: f32) -> f32 {
+	return lum.powf(3.0);
+}
+
+fn main() {
+	/*
+	let mut dir: String = "".to_string();
+	for _dir in std::fs::read_dir("/sys/class/backlight/").unwrap() {
+		dir = _dir.unwrap().path().display().to_string();
+		break;
+	}
+	*/
+
+	let backlight_path_string =
+	std::fs::read_dir("/sys/class/backlight/")
+	.unwrap()
+	.next()
+	.unwrap()
+	.unwrap()
+	.path()
+	.display()
+	.to_string();
+
+	let brightness_path_string = format!("{}/brightness", backlight_path_string);
+	let max_brightness_path_string = format!("{}/max_brightness", backlight_path_string);
+
+	let brightness =
+	std::fs::read_to_string(&brightness_path_string) // I don't really get why I need the address here
+	.unwrap()
+	.trim_end()
+	.to_string()
+	.parse::<f32>()
+	.unwrap();
+
+	let max_brightness =
+	std::fs::read_to_string(max_brightness_path_string) // Do I need to use the address here?
+	.unwrap()
+	.trim_end()
+	.to_string()
+	.parse::<f32>()
+	.unwrap();
+
+	let args: Vec<String> = std::env::args().collect();
+
+	let mut scale_pos = brightness/max_brightness;
+	let scale_tar = args[1].parse::<f32>().unwrap().min(1.0).max(0.0);
+
+	let step_tar = (max_brightness*scale_tar).round();
+	println!("{} -> {}", scale_pos, scale_tar);
+	println!("Round target {}", step_tar);
+
+	let dt = 0.01; // Pretty much equivalent to dt because there's like zero overhead in the loop
+	let loop_duration = std::time::Duration::from_millis((1000.0*dt) as u64);
+
+	//println!("{}", brightness_path_string);
+
+	let brightness_path_buf = std::path::PathBuf::from(brightness_path_string);
+
+	loop {
+		//scale_pos += dt*(scale_tar - scale_pos);
+		scale_pos = constant_speed_interpolation(scale_pos, scale_tar, 0.1, dt);
+		println!("{}", scale_pos);
+
+		let step_pos = (max_brightness*scale_pos).round();
+		std::fs::write(&brightness_path_buf, step_pos.to_string()).unwrap();
+
+		if step_pos == step_tar {
+			break;
+		}
+
+		std::thread::sleep(loop_duration);
+	}
+}
